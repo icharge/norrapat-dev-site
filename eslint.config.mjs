6c1720ec8812e4ee3052a8bdf31953f48bab9cca
NorrapatN <charge_n@hotmail.com>
2025-01-07 13:06:55 +0700

    chore: Add cf-pages support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import nextOnPages from "eslint-plugin-next-on-pages";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +11,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    plugins: {
+      "next-on-pages": nextOnPages,
+    },
+  },
+  ...nextOnPages.configs["recommended"],
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import PortfolioPage from "@/components/PortfolioPage";
 
+export const runtime = "edge";
+
 export default function Home() {
   return <PortfolioPage />;
 }
