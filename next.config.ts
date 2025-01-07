70504d0042b97fdba1c90f6536f054ca0adc0e79
NorrapatN <charge_n@hotmail.com>
2025-01-07 13:24:55 +0700

    chore: Cloudflare Pages fixes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+import initializeBundleAnalyzer from "@next/bundle-analyzer";
+
+// https://www.npmjs.com/package/@next/bundle-analyzer
+const withBundleAnalyzer = initializeBundleAnalyzer({
+  enabled: process.env.BUNDLE_ANALYZER_ENABLED === "true",
+});
+
+// https://nextjs.org/docs/pages/api-reference/next-config-js
 const nextConfig: NextConfig = {
+  output: "standalone",
   reactStrictMode: true,
   compiler: {
     // This is important for framer-motion to work properly
@@ -8,4 +18,10 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default async function config() {
+  if (process.env.NODE_ENV === "development") {
+    await setupDevPlatform();
+  }
+
+  return withBundleAnalyzer(nextConfig);
+}
